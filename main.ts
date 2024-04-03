1c7947496060e44a6ec41c251a06cb97cb27ae16
loudandrey <165939365+loudandrey@users.noreply.github.com>
2024-04-03 14:32:39 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . # # . .
+        # # # . .
+        . # # # #
+        . # # # .
+        `)
+    basic.pause(500)
+})
